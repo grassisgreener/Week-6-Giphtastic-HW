f0eebfeb1b4180c4887d7054e8abc19c36428fcf
grassisgreener <vany@uw.edu>
2017-09-14 10:12:58 -0500

    this

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -47,7 +47,7 @@ $(document).ready(function()
 		$('#gifView').empty();
 		var topic = $(this).attr('data-name');
 		console.log(topic);
-		var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + topics + "&api_key=dc6zaTOxFJmzC&limit=10";
+		var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topics + "&api_key=dc6zaTOxFJmzC&limit=10";
 
 		$.ajax({url: queryURL, method: 'GET'})
 		.done(function(response)
